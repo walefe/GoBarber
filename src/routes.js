b79f93e504b28139de0f4ac969fc671e7b5e66f8
Walefe <walefedev@gmail.com>
2019-08-20 15:02:33 -0300

    Chore: Add eslint, .editorconfig and prettier to code.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello RocketSeat" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello RocketSeat' });
 });
 
 export default routes;
