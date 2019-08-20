1431a98f1aba5e705beb8ad0b8c96a18ece5c7db
Walefe <walefedev@gmail.com>
2019-08-20 15:19:17 -0300

    Chore: Add config sequelize and database.js file.

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialiect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
