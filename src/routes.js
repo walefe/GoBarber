7bf96c4e9163d415b326b73c88778af28988057e
Walefe <walefedev@gmail.com>
2019-08-20 16:22:07 -0300

    Connect database

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello RocketSeat' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'walefe',
+    email: 'walefe@rocketseat.com',
+    password_hash: '123456789',
+  });
+  return res.json(user);
 });
 
 export default routes;
