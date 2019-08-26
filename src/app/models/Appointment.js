c465e5e9ceb7afd9ca7485752e0a9232645df643
Walefe <walefedev@gmail.com>
2019-08-26 08:39:23 -0300

    Chore: User Schedules List.

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -17,7 +17,7 @@ class Appointment extends Model {
 
   static associate(models) {
     this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
-    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'porvider' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
   }
 }
 
