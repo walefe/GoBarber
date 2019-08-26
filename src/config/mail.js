692db2bbf524d6f0b800d4de087ee103aa6d0529
Walefe <walefedev@gmail.com>
2019-08-26 15:07:27 -0300

    Chore: send email to inform you that the appointment has been canceled.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'eb27f6d89da218',
+    pass: '200b4442998946',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
