cbdc7755ae788452d4ab76fb797e10c7f97e8c80
Walefe <walefedev@gmail.com>
2019-08-26 08:28:09 -0300

    Chore: Add date validate at appointments.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
-import Appointment from '../models/Appointment';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
+import Appointment from '../models/Appointment';
 
 class AppointmentController {
   async store(req, res) {
@@ -26,6 +27,32 @@ class AppointmentController {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
+    }
+
+    /**
+     * Check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    /**
+     * Check date availability
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
     }
 
     const appointment = await Appointment.create({
